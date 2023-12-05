7b657d575f6f86c43f3b0c2bfd272cd48de977e9
Serhii Demchenko <svdemchenko@gmail.com>
2023-12-05 23:02:31 +0200

    add backend

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Post from "./Post";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
@@ -6,8 +6,28 @@ import classes from "./PostsList.module.css";
 
 const PostsList = ({ isPosting, onStopPosting }) => {
 	const [posts, setPosts] = useState([]);
+	const [isFetching, setIsFetching] = useState(false);
+
+	useEffect(() => {
+		async function fetchPosts() {
+			setIsFetching(true);
+			const response = await fetch("http://localhost:8080/posts");
+			const resData = await response.json();
+			setPosts(resData.posts);
+			setIsFetching(false);
+		}
+
+		fetchPosts();
+	}, []);
 
 	function addPostHandler(postData) {
+		fetch("http://localhost:8080/posts", {
+			method: "POST",
+			body: JSON.stringify(postData),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
 		setPosts((prevPosts) => [postData, ...prevPosts]);
 	}
 
@@ -18,7 +38,7 @@ const PostsList = ({ isPosting, onStopPosting }) => {
 					<NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
 				</Modal>
 			)}
-			{posts.length > 0 && (
+			{!isFetching && posts.length > 0 && (
 				<ul className={classes.posts}>
 					{posts.map((post) => (
 						<Post key={post.author} author={post.author} body={post.body} />
@@ -26,10 +46,15 @@ const PostsList = ({ isPosting, onStopPosting }) => {
 				</ul>
 			)}
 
-			{posts.length === 0 && (
+			{!isFetching && posts.length === 0 && (
 				<div style={{ textAlign: "center", color: "white" }}>
 					<h2>There are no posts yet</h2>
 					<p>Start adding some!</p>
+				</div>
+			)}
+			{isFetching && (
+				<div style={{ textAlign: "center", color: "white" }}>
+					<p>Loading posts ...</p>{" "}
 				</div>
 			)}
 		</>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import NewPost from "./components/NewPost";
+import RootLayout from "./routes/RootLayout";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <RootLayout />,
+		children: [
+			{ path: "/", element: <App /> },
+			{ path: "create-post", element: <NewPost /> },
+		],
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
